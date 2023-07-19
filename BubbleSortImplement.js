f8019d9f1f55e9b74fa400399aa69cbf2ff8808f
Jn-Sajol <julkar10121@gmail.com>
2023-07-19 11:04:52 +0600

    bubble short implement

---
+++ BubbleSortImplement.js
@@ -0,0 +1,14 @@
+let sort = (arr) =>{
+    for(let i=0; i<arr.length; i++){
+      for(let j=0; j<arr.length-i-1; j++){
+        // console.log(arr[j],arr[j+1])
+        if(arr[j]>arr[j+1]){
+          let temp =arr[j]
+          arr[j] = arr[j+1]
+          arr[j+1]=temp;
+        }
+      }
+    }
+    return arr;
+  }
+  console.log(sort([34,-4,6,56,23,-556,344]))
