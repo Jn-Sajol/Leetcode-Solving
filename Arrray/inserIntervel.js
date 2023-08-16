131643530b60c6ef5b56d05c87f5d8261283ada5
Jn-Sajol <julkar10121@gmail.com>
2023-08-16 22:23:07 +0600

    solved inser intervel

---
+++ Arrray/inserIntervel.js
@@ -0,0 +1,25 @@
+// O(n), O(n)
+var insert = function (intervals, newInterval) {
+    let [start, end] = newInterval;
+    let left = [];
+    let right = [];
+    
+    for (const interval of intervals) {
+      const [first, last] = interval;
+      
+      // current interval is smaller than newInterval
+      if (last < start) left.push(interval);
+      
+      // current interval is larger than newInterval
+      else if (first > end) right.push(interval);
+      
+      // there is a overlap
+      else {
+        start = Math.min(start, first);
+        end = Math.max(end, last);
+      }
+    }
+    
+    return [...left, [start, end], ...right]; 
+  };
+  insert();
