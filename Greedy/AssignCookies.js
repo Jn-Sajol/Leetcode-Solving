d1eb4688721a06a7101e28b32303820c9d7f0a1f
Jn-Sajol <julkar10121@gmail.com>
2023-09-09 20:47:23 +0600

    soleved a easy greedy assign cookies problem

---
+++ Greedy/AssignCookies.js
@@ -0,0 +1,17 @@
+var findContentChildren = function(g, s) {
+    g.sort((a, b) => a - b);
+    
+    s.sort((a, b) => a - b);
+    
+    let res = 0;
+    let currChild = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (g[currChild] <= s[i]) {
+            res++;
+            currChild++;
+        }
+    }
+    
+    return res;
+};
