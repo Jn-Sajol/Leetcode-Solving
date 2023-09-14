df9d1e6b27ac59a3bfe6d7e10302dfca93c7e2a5
Jn-Sajol <julkar10121@gmail.com>
2023-09-14 23:43:16 +0600

    trying solve fitching array in greedy

--- Graph/FindTheTown.js
+++ Graph/FindTheTown.js
@@ -1,22 +1,9 @@
-/**
- * @param {number} n
- * @param {number[][]} trusts
- * @return {number}
- */
 var findJudge = function(n, trusts) {
     if (trusts.length == 0 && n == 1)
-        return 1;   // Town judge is the only one in the town (what a loser)
-
-    // To solve this problem, we can check each person in the array
-    // If degree in == n - 1, then property 2 is satisfied
-
-    // Needs to start at 1, since people are labeled 1 - n
+        return 1;
     let degreeArr = new Array(n + 1).fill(0); 
 
     for (let trust of trusts) {
-        // Update out degree of trust[0]
-        // Update in degree of trust[1]
-
         degreeArr[trust[0]]--; 
         degreeArr[trust[1]]++;
     }
@@ -28,3 +15,4 @@ var findJudge = function(n, trusts) {
 
     return -1;
 };
+findJudge();
