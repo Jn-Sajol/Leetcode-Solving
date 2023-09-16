1ff023ea4908e16edfa46b6a8d1ad14bc83cd985
Jn-Sajol <julkar10121@gmail.com>
2023-09-16 19:42:16 +0600

    ttrying to solve clone graph

---
+++ Graph/CloneGraph.js
@@ -0,0 +1,15 @@
+// DFS approach
+var cloneGraph = function (node) {
+    if (!node) return null;
+
+    let dfs = (node, visited) => {
+        if (visited.has(node)) return visited.get(node);
+        let newNode = new Node(node.val);
+        visited.set(node, newNode);
+        for (let neighbor of node.neighbors) {
+            newNode.neighbors.push(dfs(neighbor, visited));
+        }
+        return newNode;
+    }
+    return dfs(node, new Map());
+}
