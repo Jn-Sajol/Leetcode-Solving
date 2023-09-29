a681a9a68483a342545b8df9dc129a5f27f3443b
Jn-Sajol <julkar10121@gmail.com>
2023-09-29 21:10:41 +0600

    solved

--- Bainary Tree/homeRubber.js
+++ Bainary Tree/homeRubber.js
@@ -1,15 +1,4 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
+
 var rob = function(root) {
     
     function dfs(root){
@@ -34,3 +23,4 @@ var rob = function(root) {
 
 
 };
+rob()
