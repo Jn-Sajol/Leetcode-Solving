24205574c0f5058da5e61566e31152ae17f1f5d2
Jn-Sajol <julkar10121@gmail.com>
2023-10-04 22:13:55 +0600

    dynamic programing easy try

--- Dynamic Programing/editDistance.js
+++ Dynamic Programing/editDistance.js
@@ -9,4 +9,15 @@ var minDistance = function(word1, word2) {
         dp[0][i] = i
     }
 
+    for (let i = 1;i<dp.length;i++) {
+        for (let j=1;j<dp[0].length;j++) {
+            dp[i][j] = Math.min(
+                            dp[i-1][j]+1, // left
+                            dp[i][j-1]+1, // right
+                            dp[i-1][j-1] + (word1[i-1]!=word2[j-1]?1:0) // diagonal
+                        );
+        }
+    }
+    return dp[dp.length-1][dp[0].length-1];
 };
+minDistance()
