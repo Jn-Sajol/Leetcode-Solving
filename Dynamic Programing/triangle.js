64d1b17dddc874910ec8e43182573d247a8303e3
Jn-Sajol <julkar10121@gmail.com>
2023-10-05 22:34:57 +0600

    solving pasclage triangle

--- Dynamic Programing/triangle.js
+++ Dynamic Programing/triangle.js
@@ -1,7 +1,4 @@
-/**
- * @param {number[][]} triangle
- * @return {number}
- */
+
 var minimumTotal = function(triangle) {
     for (let i = triangle.length-2; i >= 0; i--)
         for (let j = 0; j < triangle[i].length; j++)
