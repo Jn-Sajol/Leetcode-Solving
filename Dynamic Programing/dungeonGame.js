2294aec0df08bb89e4c6dbf3906238f20d3b8865
Jn-Sajol <julkar10121@gmail.com>
2023-10-05 22:37:41 +0600

    solving pasclage triangle

--- Dynamic Programing/dungeonGame.js
+++ Dynamic Programing/dungeonGame.js
@@ -10,5 +10,12 @@ var calculateMinimumHP = function(dungeon) {
     
     dp[n][m-1] = dp[n-1][m] = 1;
     
- 
+    for(let i = n-1; i >= 0; i--){
+        for (let j = m-1; j >= 0; j--){
+            let minn = Math.min(dp[i+1][j], dp[i][j+1]);
+            dp[i][j] = Math.max(minn - dungeon[i][j], 1)
+        }
+    }
+    
+    return dp[0][0]
 };
