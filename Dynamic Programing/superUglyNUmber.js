cf831b286170791a9599c3be2d26735f181ef632
Jn-Sajol <julkar10121@gmail.com>
2023-10-10 23:16:16 +0600

    solving super ugly number medium

--- Dynamic Programing/superUglyNUmber.js
+++ Dynamic Programing/superUglyNUmber.js
@@ -7,5 +7,10 @@ var nthSuperUglyNumber = function(n, primes) {
     var arrayN = new Array(n);
     var primesIndex = new Array(primes.length).fill(0);
     arrayN[0] = 1;
- 
+    for (var i = 1; i < n; i++) {
+        var min = Number.MAX_VALUE;
+        for (var j = 0; j < primes.length; j++) {
+            min = Math.min(min, primes[j] * arrayN[primesIndex[j]]);
+        }
+      
 };
