17fd956be36d6a52cdaa34a298527a0f7793e85d
Jn-Sajol <julkar10121@gmail.com>
2023-10-10 23:14:02 +0600

    solving perfect squire medum

--- Dynamic Programing/perfectSquire.js
+++ Dynamic Programing/perfectSquire.js
@@ -1,5 +1,9 @@
 function numSquares(n) {
     let dp = Array(n+1).fill(Infinity);
     dp[0] = 0; // number of perfect square to get here;
-   
+    for(let g=1; g<=n; g++){
+        for(let k=1; k*k<=g; k++){
+            dp[g] = Math.min(dp[g], dp[g - (k*k)] + 1) 
+        }
+    }
 }
