7f2fba1b7351a9f6e8ebe83d06dfb6bff027b808
Jn-Sajol <julkar10121@gmail.com>
2023-10-10 23:11:22 +0600

    solving number of one digit one hard one

--- Dynamic Programing/numberOFDigitOne.js
+++ Dynamic Programing/numberOFDigitOne.js
@@ -4,5 +4,9 @@
  */
 var countDigitOne = function(n) {
     let count = 0;
-  
+    for (let i = 1; i <= n; i *= 10) {
+      const d = i * 10;
+      const q = Math.floor(n / d);
+      const r = n % d;
+   
 };
