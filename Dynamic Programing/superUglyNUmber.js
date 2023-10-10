11d648e6f58d84dff13dccfc2399eba6170aa8d4
Jn-Sajol <julkar10121@gmail.com>
2023-10-10 23:15:57 +0600

    solving super ugly number medium

---
+++ Dynamic Programing/superUglyNUmber.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @param {number[]} primes
+ * @return {number}
+ */
+var nthSuperUglyNumber = function(n, primes) {
+    var arrayN = new Array(n);
+    var primesIndex = new Array(primes.length).fill(0);
+    arrayN[0] = 1;
+ 
+};
