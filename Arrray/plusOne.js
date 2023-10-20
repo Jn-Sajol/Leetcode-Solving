74157ab04abe0b78ec9d508ed10367c99476edd0
Jn-Sajol <julkar10121@gmail.com>
2023-10-20 15:45:27 +0600

    permutation trying to solve

--- Arrray/plusOne.js
+++ Arrray/plusOne.js
@@ -5,7 +5,5 @@
 var plusOne = function(digits) {
     let s = digits.join("");
     let n = BigInt(s) + 1n;
-    
-    // Convert the result back to an array of digits
     return n.toString().split("");
   };
