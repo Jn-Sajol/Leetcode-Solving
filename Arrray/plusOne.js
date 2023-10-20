63b830f6a02ccb81fa4b7016193d4c8c1b7696e0
Jn-Sajol <julkar10121@gmail.com>
2023-10-20 15:45:18 +0600

    permutation trying to solve

--- Arrray/plusOne.js
+++ Arrray/plusOne.js
@@ -4,9 +4,6 @@
  */
 var plusOne = function(digits) {
     let s = digits.join("");
-    
-    // Increment the number represented by the string by 1
-    // Using BigInt and 1n to remove the hassle of big integer
     let n = BigInt(s) + 1n;
     
     // Convert the result back to an array of digits
