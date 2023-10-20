64acfc38af5a7fd32930b1c73d964fca17d54d93
Jn-Sajol <julkar10121@gmail.com>
2023-10-20 15:19:58 +0600

    permutation trying to solve

---
+++ Arrray/permutation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = (nums) => {
+    // Backtracking
+    const used = new Set(); // Keep track of what we have used
+    const path = []; // Current potiential answer array
+    const res = []; // Result array to be returned
+    
+   
+}
