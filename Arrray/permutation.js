aed22eea84491966a38f59690f9da44e9b12ccd7
Jn-Sajol <julkar10121@gmail.com>
2023-10-20 15:22:06 +0600

    permutation trying to solve

--- Arrray/permutation.js
+++ Arrray/permutation.js
@@ -36,3 +36,32 @@ const permute = (nums) => {
     return res;
 }
 permute()
+
+/**46. Permutations
+Medium
+18.1K
+292
+Companies
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique. */
