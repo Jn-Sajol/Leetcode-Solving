30f9ef602a91d134c2dca53cfe652be58e166221
Jn-Sajol <julkar10121@gmail.com>
2023-10-21 23:47:26 +0600

    solving hastable

--- HashTable/threeSum.js
+++ HashTable/threeSum.js
@@ -3,10 +3,6 @@ function threeSum(nums) {
 
 	// obviously irrelevant if we don't have at least 3 numbers to play with!
 	if (nums.length < 3) return results
-
-	// having the numbers in ascending order will make this problem much easier.
-	// also, knowing the overall problem  will take at least O(N^2) time, we can
-	// afford the O(NlogN) sort operation
 	nums = nums.sort((a, b) => a - b)
 	let target = 0
 
