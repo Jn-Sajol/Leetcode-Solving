2af93fb07ab2acd40709fb30218b79e432c2a25f
Jn-Sajol <julkar10121@gmail.com>
2023-10-21 23:43:48 +0600

    solving hastable

--- HashTable/threeSum.js
+++ HashTable/threeSum.js
@@ -12,5 +12,16 @@ function threeSum(nums) {
     // if the question asks us for a custom target, we can control it here
 	let target = 0
 
+	for (let i = 0; i < nums.length - 2; i++) {
+		// `i` represents the "left" most number in our sorted set.
+		// once this number hits 0, there's no need to go further since
+		// positive numbers cannot sum to a negative number
+		if (nums[i] > target) break
+
+		// we don't want repeats, so skip numbers we've already seen
+		if (i > 0 && nums[i] === nums[i - 1]) continue
+
 	
+
+	return results
 };
