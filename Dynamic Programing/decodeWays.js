b3fd6beb700e5a24e4cd03f36369c377e08f0e43
Jn-Sajol <julkar10121@gmail.com>
2023-10-24 23:46:27 +0600

    dynamic solving

--- Dynamic Programing/decodeWays.js
+++ Dynamic Programing/decodeWays.js
@@ -3,5 +3,15 @@ var numDecodings = s => {
     dp[0] = 1;
     dp[1] = s[0] === '0' ? 0 : 1;
     
-
+    for(let i = 2, n = s.length + 1; i < n; i++) {
+        const twoDigit = Number(s.substring(i - 2, i));
+        
+        if (Number(s.substring(i - 1, i)) > 0)
+            dp[i] += dp[i - 1];
+        
+        if (twoDigit > 9 && twoDigit < 27)
+            dp[i] += dp[i - 2];
+    }
+    
+    return dp.pop();
 };
