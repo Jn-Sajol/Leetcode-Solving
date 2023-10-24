7253d3ce18c7ffd7e8072414d3fc365223d24251
Jn-Sajol <julkar10121@gmail.com>
2023-10-24 23:49:00 +0600

    dynamic solving

--- Dynamic Programing/besttimeforsellbuy.js
+++ Dynamic Programing/besttimeforsellbuy.js
@@ -2,5 +2,13 @@
 var maxProfit = function(prices) {
     let buy = prices[0] 
     let maxProfit = 0;
-  
+    for(let i=0; i<prices.length; i++) {
+        if(prices[i]-buy > maxProfit) {
+            maxProfit = prices[i]-buy
+        }
+        if(prices[i] < buy) {
+            buy = prices[i]
+        }
+      }
+      return maxProfit
  };
