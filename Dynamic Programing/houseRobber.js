d9a1d90259608fc513e3766770c3d89768a537d4
Jn-Sajol <julkar10121@gmail.com>
2023-10-24 23:51:08 +0600

    dynamic solving

--- Dynamic Programing/houseRobber.js
+++ Dynamic Programing/houseRobber.js
@@ -2,5 +2,12 @@ var rob = function(nums) {
     if (nums.length == 1) return nums[0];
     let n= nums.length;
 
-
+    let p1 = nums[0], p2 = Math.max(nums[1], p1);
+    let maxVal = 0;
+    for (let i = 2; i < n; i++) {
+        maxVal = Math.max(p1+nums[i], p2);
+        p1 = p2;
+        p2 = maxVal;
+    }
+    return p2;
 };
