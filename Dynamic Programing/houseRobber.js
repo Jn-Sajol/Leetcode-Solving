fbc5de0515eaba87819ca94fac2b20f84bd021e2
Jn-Sajol <julkar10121@gmail.com>
2023-10-24 23:51:59 +0600

    dynamic solving

--- Dynamic Programing/houseRobber.js
+++ Dynamic Programing/houseRobber.js
@@ -31,4 +31,9 @@ Example 2:
 Input: nums = [2,7,9,3,1]
 Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-Total amount you can rob = 2 + 9 + 1 = 12. */
+Total amount you can rob = 2 + 9 + 1 = 12.
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400*/
