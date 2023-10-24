d1000fed0afbbb58268b277060845ac98c4cea86
Jn-Sajol <julkar10121@gmail.com>
2023-10-24 23:53:30 +0600

    dynamic solving

--- Dynamic Programing/Coinchange.js
+++ Dynamic Programing/Coinchange.js
@@ -3,3 +3,10 @@ var coinChange = function(coins, amount) {
     return result === Infinity ? -1 : result
 };
 
+const countCoinChange = (coins, amount, memo={}) => {
+    if(amount === 0) return 0
+    if(amount < 0) return Infinity
+    if(memo[amount] !== undefined) return memo[amount]
+
+
+}
