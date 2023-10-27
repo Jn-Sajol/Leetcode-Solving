4f0256fec9ea39054178d645a2be7babacee5591
Jn-Sajol <julkar10121@gmail.com>
2023-10-27 23:34:31 +0600

    solving queue

--- Queue/firstUniqeCharacter.js
+++ Queue/firstUniqeCharacter.js
@@ -9,3 +9,31 @@ var firstUniqChar = function(s) {
 };
 
 firstUniqChar()
+
+/** First Unique Character in a String
+Easy
+8.3K
+266
+Companies
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters. */
