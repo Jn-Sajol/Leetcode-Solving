9e0e33686ff7f3d32fb7fe426941fc0cbd3da6ef
Jn-Sajol <julkar10121@gmail.com>
2023-10-27 23:33:55 +0600

    solving queue

--- Queue/firstUniqeCharacter.js
+++ Queue/firstUniqeCharacter.js
@@ -1,4 +1,9 @@
 
 var firstUniqChar = function(s) {
-
+    for(let i = 0; i < s.length; i++){
+      if(s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+          return s.indexOf(s[i])
+      }
+  }
+  return -1;
 };
