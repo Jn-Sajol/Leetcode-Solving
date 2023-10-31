923d3ed1ed4f0826cc773000324ace662231d2e7
Jn-Sajol <julkar10121@gmail.com>
2023-10-31 22:05:28 +0600

    palindrome

--- LinkList/palindrom.js
+++ LinkList/palindrom.js
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
+
 const isPalindrome = function(head) {
     let palindrome = [];
     <!-- this pushes the linked list into the empty array[] -->
