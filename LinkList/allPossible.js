c0a2ce34703a250c27bc26642d6b460395f8ae55
Jn-Sajol <julkar10121@gmail.com>
2023-10-31 22:14:54 +0600

    palindrome

--- LinkList/allPossible.js
+++ LinkList/allPossible.js
@@ -16,5 +16,13 @@ var allPossibleFBT = function(n) {
     if(dp[n]!=-1)
         return dp[n];
     let ret = [];
- 
+    for(let i=1;i<n;i+=2){
+        let ltrees = allPossibleFBT(i);
+        let rtrees = allPossibleFBT(n-i-1);
+        for(let j=0;j<ltrees.length;j++)
+            for(let k=0;k<rtrees.length;k++)
+                ret.push(new TreeNode(0,ltrees[j],rtrees[k]));
+    }
+    dp[n]=ret;
+    return ret;
 };
