d62fe4de946bc35d7ef8094a62e9ce4e1653901e
Jn-Sajol <julkar10121@gmail.com>
2023-10-31 22:05:54 +0600

    palindrome

--- LinkList/palindrom.js
+++ LinkList/palindrom.js
@@ -1,13 +1,11 @@
 
 const isPalindrome = function(head) {
     let palindrome = [];
-    <!-- this pushes the linked list into the empty array[] -->
     while (head) {
       palindrome.push(head.val);
       head = head.next;
     }
     
-<!-- This checks if the value is a palindrome or not -->
     for (let i = 0, j = palindrome.length - 1; i < j; i++, j--) {
       if (palindrome[i] !== palindrome[j]) {
         return false;
