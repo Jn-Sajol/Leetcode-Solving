6bf158ab6bdfe0d4738c5e067a900946a5329417
Jn-Sajol <julkar10121@gmail.com>
2023-10-31 22:22:22 +0600

    palindrome

--- LinkList/removeNodes.js
+++ LinkList/removeNodes.js
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
+
 var removeNodes = function(head) {
     
     if(!head) return null
