7249336907b39cb548dc7e19cac647d06ae703a7
Jn-Sajol <julkar10121@gmail.com>
2023-10-31 22:15:14 +0600

    palindrome

--- LinkList/allPossible.js
+++ LinkList/allPossible.js
@@ -1,15 +1,4 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {number} n
- * @return {TreeNode[]}
- */
+
 const dp = new Array(21).fill(-1);
 dp[1] = [new TreeNode()];
 var allPossibleFBT = function(n) {
