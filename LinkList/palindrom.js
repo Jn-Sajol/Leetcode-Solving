563833ccd5d9e5c264e309d0b62e03e768d13ff3
Jn-Sajol <julkar10121@gmail.com>
2023-10-31 22:05:12 +0600

    palindrome

--- LinkList/palindrom.js
+++ LinkList/palindrom.js
@@ -16,7 +16,13 @@ const isPalindrome = function(head) {
       palindrome.push(head.val);
       head = head.next;
     }
-
+    
+<!-- This checks if the value is a palindrome or not -->
+    for (let i = 0, j = palindrome.length - 1; i < j; i++, j--) {
+      if (palindrome[i] !== palindrome[j]) {
+        return false;
+      }
+    }
     
     return true;
   };
