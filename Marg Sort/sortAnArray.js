3012edea7dbf5978d119ae5a9280e597e6f60da2
Jn-Sajol <julkar10121@gmail.com>
2023-11-01 22:29:24 +0600

    addong marge sortt

---
+++ Marg Sort/sortAnArray.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    let compare = (a,b)=>{
+        return a-b;
+    }
+    return nums.sort(compare);
+    
+};
